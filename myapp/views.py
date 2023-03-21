35d8c5256d96dc6b37da7c0f998456c1659ba3de
Akhil Vijayakumar <akhilvijayakumar333@gmail.com>
2023-03-22 00:21:49 +0530

    imageuploader

---
+++ myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,redirect
+from .forms import ImageForm
+from .models import Image
+
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        form= ImageForm(request.POST,request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    form = ImageForm
+    img=Image.objects.all()
+    context = {'form':form,'img':img}
+    return render(request,'myapp/home.html',context)
